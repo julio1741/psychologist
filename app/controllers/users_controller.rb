1ec93c4f04a236098fe3675a7e89ed86ab1ac735
Julio Baptista <julio.baptista@routinguc.com>
2022-07-14 01:08:23 -0400

    Add users resources

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,5 @@
 class UsersController < ApplicationController
   def index
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def show
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
+    @users = User.all
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  validates :username, :email, presence: true
+  validates :username, :email, uniqueness: true
+
+  def self.from_omniauth(auth)
+    #Creates a new user only if it doesn't exist
+    where(email: auth.info.email).first_or_initialize do |user|
+      user.username = auth.info.name
+      user.email = auth.info.email
+      if auth.info.uid
+        user.uid = auth.info.uid
+      end
+      user.password = SecureRandom.hex
+    end
+  end
 end
