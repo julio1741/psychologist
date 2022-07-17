beeac619798f9f090c2dbe56b78b5d9d34222ce2
Julio Baptista <julio.baptista@routinguc.com>
2022-07-17 15:55:26 -0400

    Filter block times by availability

--- app/helpers/doctors_helper.rb
+++ app/helpers/doctors_helper.rb
@@ -3,7 +3,10 @@ module DoctorsHelper
     day_date = Date.parse(day).strftime("%A").downcase
     doctor_working_day = get_selected_work_day(day_date)
     return [[], nil] if doctor_working_day.nil?
-    [@doctor.block_times, doctor_working_day]
+    # remove reserved block times
+    reservations = Reservation.joins(:doctor, :block_time, :work_day).where(doctor_id: @doctor.id, work_day: {name: day_date})
+    reserved_block_times = reservations.map(&:block_time)
+    [(@doctor.block_times - reserved_block_times), doctor_working_day]
   end
 
   def get_selected_work_day day
