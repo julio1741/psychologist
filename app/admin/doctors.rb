f369609f586bf25fa11ec05578bf690f1f950ba3
Julio Baptista <julio.baptista@routinguc.com>
2022-07-26 00:01:48 -0400

    Add doctors form to active admin

--- app/admin/doctors.rb
+++ app/admin/doctors.rb
@@ -1,3 +1,17 @@
 ActiveAdmin.register Doctor do
-  permit_params :firstname, :lastname, :phone, :hospital_id
+
+  permit_params :firstname, :lastname, :phone, :hospital_id, work_day_ids: [], block_time_ids: []
+
+  form do |f|
+    f.inputs 'Doctor information' do
+      f.input :firstname
+      f.input :lastname
+      f.input :phone
+      f.input :hospital_id, :as => :select, :collection => Hospital.all.map {|h| [h.name, h.id]}, :include_blank => false
+      f.input :work_days, :as => :check_boxes
+      f.input :block_times, :as => :check_boxes, member_label: Proc.new { |c| "#{c.start.strftime("%I:%M %p")} #{c.end.strftime("%I:%M %p")}" }
+    end
+
+    f.actions
+  end
 end
--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,6 +2,7 @@ class Doctor < ApplicationRecord
   belongs_to :hospital
   has_many :working_days, dependent: :destroy
   has_many :work_days, through: :working_days
+  accepts_nested_attributes_for :working_days
 
   has_many :working_hours, dependent: :destroy
   has_many :block_times, through: :working_hours
