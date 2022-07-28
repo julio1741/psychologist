3471afd111effba8ab623f430d708df0b4b5cdbd
Julio Baptista <julio.baptista@routinguc.com>
2022-07-27 20:42:21 -0400

    Recover test folder, and add login test integration

---
+++ test/login_test.rb
@@ -0,0 +1,31 @@
+require "test_helper"
+
+class LoginTest < ActionDispatch::IntegrationTest
+  fixtures :users
+
+  test "login with valid credentials" do
+    get "/login"
+    assert_equal 200, status
+    maria = login(:maria, 'maria')
+    follow_redirect!
+    assert_equal 200, status
+    assert_equal "/", path
+  end
+
+  test "login with wrong credentials" do
+    get "/login"
+    assert_equal 200, status
+    julio = login(:julio, 'juliooo')
+    assert_equal "/login", path
+    assert_select "p", "Sorry, your username or password was incorrect"   
+  end
+
+  private 
+
+  def login(user, password)
+    user = users(user)
+    # post the login and follow through to the home page
+    post "/login", params: { user: { username: user.username, password: password } }
+  end
+
+end 
