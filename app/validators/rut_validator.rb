cbfebace2142e2f4faf9c294d4bc94b1d3e0ca39
Julio Baptista <julio.baptista@routinguc.com>
2022-07-28 19:43:17 -0400

    Fix rubocop

--- app/validators/rut_validator.rb
+++ app/validators/rut_validator.rb
@@ -1,7 +1,7 @@
 class RutValidator < ActiveModel::Validator
   require 'chilean_rut'
   def validate(record)
-    unless RUT::validate(record.rut)
+    unless RUT.validate(record.rut)
       record.errors.add(:rut, 'invalid')
     end
   end
